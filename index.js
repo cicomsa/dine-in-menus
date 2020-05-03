d9f1341d2688b96d15e7bf8eed7bdc26e12f6eec
cicomsa <ioanacomsa85@gmail.com>
2020-05-03 21:42:37 +0100

    headers in - api calls

--- index.js
+++ index.js
@@ -16,6 +16,13 @@ const PORT = process.env.PORT || 4001
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
+  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE')
+  next()
+})
+
 db.authenticate()
   .then(() => console.log('Connection has been established successfully.'))
   .catch(() => console.error('Unable to connect to the database:', error))
